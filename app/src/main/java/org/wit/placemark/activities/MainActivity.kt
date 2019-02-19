93898b6df8c1cafe0498f8c52272a8c868da3d91
jeff-ntl <jeff2406n@gmail.com>
2019-02-19 23:20:53 +0000

    complete exercise 2

--- app/src/main/java/org/wit/placemark/activities/MainActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/MainActivity.kt
@@ -2,9 +2,12 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
@@ -35,5 +38,20 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
                 toast ("Please Enter a title")
             }
         }
+        //Add action bar and set title
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        this.finish()
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
