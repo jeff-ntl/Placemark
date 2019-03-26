500a0d83397eb10427a352de9bea043a3ef90e35
jeff-ntl <jeff2406n@gmail.com>
2019-03-26 16:43:06 +0000

    complete up to lab 7 (exercise not included)

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -3,16 +3,21 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.placemark.models.PlacemarkJSONStore
 import org.wit.placemark.models.PlacemarkMemStore
 import org.wit.placemark.models.PlacemarkModel
+import org.wit.placemark.models.PlacemarkStore
 
 class MainApp : Application(), AnkoLogger {
 
     //val placemarks = ArrayList<PlacemarkModel>()
-    val placemarks = PlacemarkMemStore()
+    //val placemarks = PlacemarkMemStore()
+    lateinit var placemarks: PlacemarkStore
 
     override fun onCreate() {
         super.onCreate()
+        //placemarks = PlacemarkMemStore()
+        placemarks = PlacemarkJSONStore(applicationContext)
         info("Placemark started")
         //placemarks.add(PlacemarkModel("One", "About one..."))
        // placemarks.add(PlacemarkModel("Two", "About two..."))
